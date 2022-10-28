e5bd023b7929e0e8b7a40c07e28a5c825d3c4fce
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2022-10-28 19:51:50 -0300

    Agrego clase carta

---
+++ src/main/java/trabajoPractico/Carta.java
@@ -0,0 +1,12 @@
+package trabajoPractico;
+
+public class Carta {
+
+    private String tipoDeCarta; //Luchador, Tanque, Hechicero , Carta de magia
+
+    public Carta(String tipo){
+        this.tipoDeCarta = tipo;
+    }
+
+
+}
--- src/main/java/trabajoPractico/Main.java
+++ src/main/java/trabajoPractico/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package trabajoPractico;
 
 public class Main {
     public static void main(String[] args) {
