5e650d656c8458d90c423c1a4fa1ceb123fea308
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2022-10-31 19:47:53 -0300

    Se agrega verificacion cuando se ingresa el tipo de carta

--- src/main/java/trabajoPractico/Carta.java
+++ src/main/java/trabajoPractico/Carta.java
@@ -5,32 +5,45 @@ public class Carta {
     private int ataque;
     private int defensa;
 
-
+    //Pre: El tipo de la carta debe ser Luchador, Tanque, Hechicero o Magia
+    //Post: Crea una carta con su tipo correspondiente
     public Carta(String tipo){
-        loadAtkAndDef(tipo);
-        this.tipoDeCarta = tipo;
-    }
-
-    private void loadAtkAndDef(String tipo){
-        switch (tipo){
-            case "Luchador":
-                this.ataque = 3;
-                this.defensa = 3;
-                break;
-            case "Tanque":
-                this.ataque = 0;
-                this.defensa = 6;
-                break;
-            case "Hechicero":
-                this.ataque = 6;
-                this.defensa = 0;
-                break;
-            default: //caso carta magia
-                this.ataque = 0;
-                this.defensa = 0;
+        if(esUnidadValida(tipo)){
+            loadAtkAndDef(tipo);
+            this.tipoDeCarta = tipo;
+        }
+        else{
+            throw new RuntimeException("Tipo de carta invalido");
         }
     }
 
+    //Pre: El tipo de carta debe ser valido
+    //Post: Carga el ataque y la defensa de la carta
+    private void loadAtkAndDef(String tipo){
+        switch (tipo) {
+            case "Luchador" -> {
+                this.ataque = 3;
+                this.defensa = 3;
+            }
+            case "Tanque" -> {
+                this.ataque = 0;
+                this.defensa = 6;
+            }
+            case "Hechicero" -> {
+                this.ataque = 6;
+                this.defensa = 0;
+            }
+            default -> { //caso carta magia
+                this.ataque = 0;
+                this.defensa = 0;
+            }
+        }
+    }
 
+    //Pre: -
+    //Post: Devuelve true en caso de ser un tipo de carta valido.
+    private boolean esUnidadValida(String tipo){
+        return (tipo.equals("Luchador") || tipo.equals("Tanque") ||tipo.equals("Hechicero") ||tipo.equals("Magia"));
+    }
 
 }
