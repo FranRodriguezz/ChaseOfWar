2249767030cc2f56d751e3dbab0fa8909df8e079
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2022-10-31 19:49:12 -0300

    Se agrega el metodo que nos permite mezclar el mazo, y pre y post condiciones.

--- src/main/java/trabajoPractico/MazoDeCartas.java
+++ src/main/java/trabajoPractico/MazoDeCartas.java
@@ -1,27 +1,34 @@
 package trabajoPractico;
 
 import java.util.Stack;
-
+import java.util.Collections;
 
 public class MazoDeCartas {
 
-    private final int cantidadCartas = 40;
     private final int cantidadCartasPorTipo = 10;
     public Stack<Carta> mazo = new Stack<>();
 
+    //Pre: -
+    //Post: Crea el mazo con sus unidades correspondientes, ya mezclado el mismo.
     public MazoDeCartas(){
         this.cargarUnidades();
+        this.shuffleDeck();
     }
 
+    //Pre: -
+    //Post: Carga las unidades en el mazo, 10 cartas por unidad
     private void cargarUnidades(){
         for(int i = 0; i < cantidadCartasPorTipo; i++){
-            mazo.push(new Carta("Luchador"));
-            mazo.push(new Carta("Tanque"));
-            mazo.push(new Carta("Hechicero"));
-            mazo.push(new Carta("Juga2"));
+            this.mazo.push(new Carta("Luchador"));
+            this.mazo.push(new Carta("Tanque"));
+            this.mazo.push(new Carta("Hechicero"));
+            this.mazo.push(new Carta("Juga2"));
         }
     }
 
-
-
+    //Pre: -
+    //Post: Mezcla el mazo
+    private void shuffleDeck(){
+        Collections.shuffle(this.mazo);
+    }
 }
