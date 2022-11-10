e63d1a31767be03afe3748092f7d066f80ae3334
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2022-11-10 17:31:37 -0300

    Se hace una pequeña prueba para ver si funciona el mazo y la carta

--- src/main/java/trabajoPractico/Main.java
+++ src/main/java/trabajoPractico/Main.java
@@ -3,6 +3,8 @@ package trabajoPractico;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        MazoDeCartas mazo = new MazoDeCartas();
+        Carta carta = mazo.mazo.pop();
+        System.out.println(carta.getTipo().toString());
     }
 }
